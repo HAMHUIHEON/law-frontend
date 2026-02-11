f15cb835d616481a73f47c0a13f9125de39205db
윤승미 <seungmi0723@naver.com>
2026-02-11 10:42:53 +0900

    chore: add mail-order business registration info

--- app/components/CompanyInfoFooter.tsx
+++ app/components/CompanyInfoFooter.tsx
@@ -49,7 +49,7 @@ export function CompanyInfoFooter() {
       <div style={styles.inner}>
         <p style={{ margin: 0 }}>
           윤슬 · 사업자등록번호 348-10-03174 · 통신판매업신고번호
-          제2026-부산남구-00000호
+          제2026-부산해운대-0258호
         </p>
 
         <p style={{ margin: "4px 0 0" }}>
